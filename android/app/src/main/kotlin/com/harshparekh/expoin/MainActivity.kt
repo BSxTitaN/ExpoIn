b726c76cf1421c249e223256120d5b552e04f5da
Harsh Parekh <harshparekh@Harshs-Laptop.local>
2024-01-28 14:06:10 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/harshparekh/expoin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.harshparekh.expoin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Design-System/colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppTheme {
+  static const Color mainAppColor = Color(0xfffefefe);
+  static const Color cardNormColor = Color(0xfff8f8fa);
+  static const Color iconNormColor = Color(0xffb0b0b0);
+  static const Color iconAltColor = Color(0xffc0c8cd);
+  static const Color errLightColor = Color(0xffFAD6D6);
+  static const Color errAppColor = Color(0xffF4484A);
+}
